bdc332d9d8104cabec4d47d538cf35c25efa2baf
asifbuet047 <asifbuet047@gmail.com>
2023-10-20 17:12:41 +0600

    Add a POST API to handle user add new product request. This API receives longged in user new products seven different info as string and store it in mongodb in products collection as new document

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const clientRequestHandler = express();
 const database_name = 'brandShop';
 const brands_collection_name = 'brands';
+const banner_collection_name = 'banner';
 const products_collection_name = 'products';
 
 //middlewares
 clientRequestHandler.use(cors());
 clientRequestHandler.use(express.json());
 
-
-let brandNames = null;
-let brandCountries = null;
 
 
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.2jixdw6.mongodb.net/?retryWrites=true&w=majority`;
@@ -53,41 +51,44 @@ function getTechBrandsNamesAndLogos() {
         mongoClient.close();
     }
 }
-function getTechProduct() {
+async function serverRunning() {
+    console.log("Server running");
     try {
-        mongoClient.connect()
-            .then((client) => {
-                const brandShopDB = client.db(database_name);
-                const brandShopCollection = brandShopDB.collection(products_collection_name);
-                const query = { brand_name: "Apple" };
-                brandShopCollection.findOne(query)
-                    .then((document) => {
-                        console.log(document);
+        await mongoClient.connect();
 
-                    }, (reason) => {
-                        console.log(reason);
-                    })
+        clientRequestHandler.get('/brands', async (request, response) => {
+            const brandsCollection = mongoClient.db(database_name).collection(brands_collection_name);
+            const brands = await brandsCollection.findOne();
+            response.send(brands);
+        });
 
-            })
-            .catch((reason) => console.log(reason));
+        clientRequestHandler.get('/banner', async (request, response) => {
+            const bannerCollection = mongoClient.db(database_name).collection(banner_collection_name);
+            const banner = await bannerCollection.findOne();
+            response.send(banner);
+        });
+
+        clientRequestHandler.post('/addproduct', async (request, response) => {
+            const productsCollection = mongoClient.db(database_name).collection(products_collection_name);
+            const newProduct = request.body;
+            console.log(newProduct);
+            const addedProduct = await productsCollection.insertOne(newProduct);
+            response.send(addedProduct);
+        })
 
     } finally {
-        mongoClient.close();
+
     }
 }
 
-
-getTechBrandsNamesAndLogos();
-getTechProduct();
+serverRunning().catch((reason) => console.log(reason));
 
 
 clientRequestHandler.get('/', (req, res) => {
     res.send("Brand Shop server is running");
 })
 
-clientRequestHandler.get('/brands', (req, res) => {
-    res.send(brandNames);
-})
+
 
 clientRequestHandler.listen(port, () => {
     console.log(`Brand Shop Server is running at port ${port}`);
